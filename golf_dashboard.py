d36cef70f6f9e3d35c839da8583a5daacbf21d7c
Rory Owens <40439757@live.napier.ac.uk>
2024-11-25 23:35:25 +0000

    Added form for user input, calculation for wind adjustment added for into wind and down wind

--- golf_dashboard.py
+++ golf_dashboard.py
@@ -4,6 +4,8 @@ import pandas
 import streamlit as st
 from geopy.geocoders import Nominatim
 
+wind_types = ["Into Wind", "Down Wind", "Crosswind"]
+
 def wind_dir(wind_direction):
     north = 0
     east = 90
@@ -29,6 +31,21 @@ def wind_dir(wind_direction):
             return "South"
         else:
             return "West"
+        
+def adjusted_distance(distance, wind_speed, wind_type):
+    distance = int(distance)
+    wind_speed = int(wind_speed)
+    percent = round(wind_speed)
+    if wind_type == "Into Wind":
+        result = distance + (distance/100 * percent)
+    elif wind_type == "Down Wind":
+        half_percent = (distance/100) / 2
+        result = distance - (half_percent * percent)
+    else:
+        result = distance
+    
+    return result
+        
 
 loc = Nominatim(user_agent="GetLoc")
  
@@ -45,15 +62,31 @@ api_key = "cfc54dfa691b9699aa872b7e241e0227"
 
 result = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=imperial")
 weather_data = result.json()
-print(weather_data)
 wind_speed = weather_data['wind']['speed']
 wind_direction = weather_data['wind']['deg']
 
 wind_dir_string = wind_dir(wind_direction)
 
-st.write(f"**Wind Speed** {wind_speed} MPH")
-st.write(f"**Wind Direction** {wind_direction} Degrees, This is {wind_dir_string}")
+col1, col2 = st.columns(2)
 
+with col1:
+    st.write(f"**Wind Speed** {wind_speed} MPH")
+    st.write(f"**Wind Direction** {wind_direction} Degrees, This is {wind_dir_string}")
+
+        
+with st.form("my_form"):
+    distance = st.text_input("Enter the distance", value="0")
+    wind_type = st.radio(
+    "What type of wind is it",
+    ["Into Wind", "Down Wind", "Crosswind"],
+    index=None)
+    
+    real_distance = adjusted_distance(distance, wind_speed, wind_type)
+
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write(f"Yardage: ***{distance}*** yards", f" Wind Direction: {wind_type}")
+        st.write(f"Adjusted Yardage: {real_distance}")
 
 st.caption("Golf Dashboard -Phase 1")
 
